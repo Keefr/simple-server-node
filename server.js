19b603be7bf8c703a0e2213ca4f6973c257e1af9
CBITS <cbits@Keith-Richardsons-MacBook-Pro-2.local>
2016-03-18 19:38:21 -0400

    i've changed the server.js file with a new message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Keef, This is your FIRST IOT Device Deployment!');
+  res.send('Hello Keef, This is your FIRST IOT Device Deployment!KEEP BUILDING AND DEPLOYING!');
 });
 
 var server = app.listen(80, function () {
